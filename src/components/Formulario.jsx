5e6a5a6c2bc6c256aa27300a746af813b225b5c6
misterdan100 <danielmca60@gmail.com>
2024-03-04 19:58:41 -0500

    Creating Formulario.jsx component w/ inputs city and country, further, input button

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,35 @@
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className="contenedor">
+        <form action="">
+            <div className="campo">
+                <label htmlFor="ciudad">Ciudad</label>
+                <input 
+                type="text" 
+                id="ciudad"
+                name="ciudad"
+                />
+            </div>
+            <div className="campo">
+                <label htmlFor="pais">Pais</label>
+                <select name="pais" id="pais">
+                    <option value="">Seleccione un pais</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="CO">Colombia</option>
+                    <option value="MX">Mexico</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Peru</option>
+                </select>
+            </div>
+
+            <input type="submit" 
+                value='Consultar Clima'
+            />
+        </form>
+    </div>
   )
 }
 
