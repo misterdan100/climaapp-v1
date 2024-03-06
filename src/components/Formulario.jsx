10803a346806d62e5b614ade656569b02ab889fc
misterdan100 <danielmca60@gmail.com>
2024-03-05 22:31:47 -0500

    Creating search by country with lists of countries and cities from api

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,68 +1,67 @@
-import { useState } from "react"
-import useClima from "../hooks/useClima"
-
-
+import { useState } from "react";
+import useClima from "../hooks/useClima";
 
 const Formulario = () => {
-    const [ alerta, setAlerta ] = useState('')
-    const { busqueda, datosBusqueda, consultarClima } = useClima()
-    const { ciudad, pais } = busqueda
+  const [alerta, setAlerta] = useState("");
+  const { busqueda, datosBusqueda, consultarClima, countries, cities } =
+    useClima();
+  const { ciudad, pais } = busqueda;
 
-    const handleSubmit = (e) => {
-        e.preventDefault() 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if(Object.values(busqueda).includes('')) {
-            setAlerta('Todos los campos son obligatorios')
-            return
-        }
-
-        consultarClima(busqueda)
-
-        setAlerta('')
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Todos los campos son obligatorios");
+      return;
     }
+
+    consultarClima(busqueda);
+
+    setAlerta("");
+  };
+
+  console.log(cities)
 
   return (
     <div className="contenedor">
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="campo">
+          <label htmlFor="pais">Pais</label>
+          <select name="pais" id="pais" onChange={datosBusqueda} value={pais}>
+            <option value="">Seleccione un pais</option>
+            {countries?.map((country) => (
+              <option key={country.iso2} value={country.iso2}>
+                {country.country}
+              </option>
+            ))}
+          </select>
+        </div>
 
-        { alerta && (<p>{alerta}</p>)}
-        <form 
-            onSubmit={handleSubmit}
-        >
-            <div className="campo">
-                <label htmlFor="ciudad">Ciudad</label>
-                <input 
-                    type="text" 
-                    id="ciudad"
-                    name="ciudad"
-                    onChange={datosBusqueda}
-                    value={ciudad}
-                />
-            </div>
-            <div className="campo">
-                <label htmlFor="pais">Pais</label>
-                <select 
-                    name="pais" 
-                    id="pais"
-                    onChange={datosBusqueda}
-                    value={pais}
-                >
-                    <option value="">Seleccione un pais</option>
-                    <option value="US">Estados Unidos</option>
-                    <option value="CO">Colombia</option>
-                    <option value="MX">Mexico</option>
-                    <option value="AR">Argentina</option>
-                    <option value="CR">Costa Rica</option>
-                    <option value="ES">España</option>
-                    <option value="PE">Peru</option>
-                </select>
-            </div>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          {/* <input
+            type="text"
+            id="ciudad"
+            name="ciudad"
+            onChange={datosBusqueda}
+            value={ciudad}
+          /> */}
 
-            <input type="submit" 
-                value='Consultar Clima'
-            />
-        </form>
+          <select name="ciudad" id="ciudad" onChange={datosBusqueda} value={ciudad}>
+            <option value="">Seleccione una ciudad</option>
+            {cities?.map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <input type="submit" value="Consultar Clima" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,69 +1,100 @@
-import { createContext, useState } from "react"
-import axios from 'axios'
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const ClimaContext = createContext();
 
-const ClimaProvider = ({children}) => {
-    const [ busqueda, setBusqueda ] = useState({
-      ciudad: '',
-      pais: ''
-    })
+const ClimaProvider = ({ children }) => {
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
 
-    const [ resultado, setResultado ] = useState({})
-    const [ cargando, setCargando ] = useState(false)
-    const [ noResultado, setNoResultado ] = useState('')
-    
-    const datosBusqueda = e => {
-      setBusqueda({
-        ...busqueda,
-        [e.target.name]: e.target.value
-      })
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+  const [noResultado, setNoResultado] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [cities, setCities] = useState([])
+
+  const datosBusqueda = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+    console.log(busqueda)
+  };
+
+  const consultarClima = async (datos) => {
+    setCargando(true);
+    setNoResultado(false);
+    try {
+      const { ciudad, pais } = datos;
+
+      const appId = import.meta.env.VITE_API_KEY;
+
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+
+      const { data } = await axios(url);
+      const { lat, lon } = data[0];
+
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+      const { data: clima } = await axios(urlClima);
+      setResultado(clima);
+    } catch (error) {
+      setNoResultado("No hay resultados!");
+    } finally {
+      setCargando(false);
     }
+  };
 
-    const consultarClima = async datos => {
-      setCargando(true)
-      setNoResultado(false)
-      try {
-        const { ciudad, pais } = datos
+  const getCountries = async () => {
+    const url = "https://countriesnow.space/api/v0.1/countries";
 
-        const appId = import.meta.env.VITE_API_KEY
-
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
-
-        const { data } = await axios(url)
-        const { lat, lon } = data[0]
-
-        const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
-
-        const { data: clima } = await axios(urlClima)
-        setResultado(clima)
-      } catch( error ) {
-        setNoResultado('No hay resultados!')
-      } finally {
-        setCargando(false)
-      }
-
+    try {
+      const response = await axios(url);
+      // console.log(response.data.data)
+      setCountries(response.data.data);
+    } catch (error) {
+      console.log("Error en getCountries........", error);
     }
+  };
 
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const getCities = async country => {
+    const citiesArray = countries.filter(actual => actual.iso2 === country)
+    setCities(citiesArray[0].cities)
+    // console.log('cities >>>>>>>>>>>', country)
+    console.log('cities >>>>>>>>>>>', citiesArray[0].cities)
+  }
+
+  useEffect(() => {
+    console.log('buscando ......', busqueda.pais)
+    getCities(busqueda.pais)
+  }, [busqueda])
 
   return (
     <ClimaContext.Provider
-        value={{
-            busqueda,
-            setBusqueda,
-            datosBusqueda,
-            consultarClima,
-            resultado,
-            cargando,
-            setCargando,
-            noResultado
-        }}
+      value={{
+        busqueda,
+        setBusqueda,
+        datosBusqueda,
+        consultarClima,
+        resultado,
+        cargando,
+        setCargando,
+        noResultado,
+        countries,
+        cities
+      }}
     >
-        {children}
+      {children}
     </ClimaContext.Provider>
-  )
-}
+  );
+};
 
-export {ClimaProvider}
+export { ClimaProvider };
 
-export default ClimaContext
+export default ClimaContext;
